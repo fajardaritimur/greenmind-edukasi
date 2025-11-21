98928a3bae502904e1465a87fb4ecdbc729542b3
fajardaritimur <akunsosmedbibie@gmail.com>
2025-11-21 10:29:32 +0700

    Add files via upload

--- greenmind/assets/css/js/main.js
+++ greenmind/assets/css/js/main.js
@@ -1,1 +1,5 @@
-
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+    document.body.classList.toggle("sidebar-open");
+}
